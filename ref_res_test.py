5f6ee27f9351024773d822594c2db42a656d998f
claytonfields <claytonfields@u.boisestate.edu>
2023-10-03 09:52:55 -0600

    sync repos

--- ref_res_test.py
+++ ref_res_test.py
@@ -6,38 +6,25 @@ Created on Sat Apr 22 12:53:12 2023
 @author: claytonfields
 """
 
-import random
-import io
-import pyarrow as pa
-import os
+
 import copy
 import pytorch_lightning as pl
-from sacred import Experiment
-from PIL import Image
+
 from tqdm import tqdm
 import numpy as np
-import skimage.io as skio
-import matplotlib.pyplot as plt
 from refer import REFER
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from torch.optim import AdamW
-from torchvision import transforms
 
 from transformers import ElectraTokenizer
 
-from refcoco_utils import get_bounded_subimage
 from refcoco_utils import _config
-from refcoco_utils import _loss_names
-# from refcoco_utils import RefcocoDataset
+from refcoco_utils_test import RefcocoDataset
 
-from meter.transforms import keys_to_transforms
-from meter.config import ex
+
 from meter.modules import METERTransformerSS
-from meter.datamodules.multitask_datamodule import MTDataModule
-from meter.datasets.base_dataset import BaseDataset
+
 
 data_root = '/home/claytonfields/nlp/code/data/coco'  # contains refclef, refcoco, refcoco+, refcocog and images
 dataset = 'refcoco' 
@@ -52,72 +39,7 @@ pl.seed_everything(_config["seed"])
 
 model = METERTransformerSS(_config)
 
-# RefCOCO data class
-class RefcocoDataset(torch.utils.data.Dataset):
 
-    def __init__(self, refer, tokenizer, split='', max_bb = 75):
-        self.tokenizer = tokenizer
-        self.refer = refer
-        self.max_bb = max_bb
-        self.split = split
-        self.sent_ids = self.get_sent_ids()[:50]
-        
-
-    def __len__(self):
-        return len(self.sent_ids)
-    
-    def get_sent_ids(self):
-        sent_ids = []
-        for ref_id in self.refer.getRefIds(split=self.split):
-            ref = self.refer.Refs[ref_id]
-            for sent_id in ref['sent_ids']:
-                sent_ids.append(sent_id)
-        return sent_ids
-
-    def __getitem__(self, index):
-        sent_id = self.sent_ids[index]
-        ref = self.refer.sentToRef[sent_id]
-        sent = self.refer.Sents[sent_id]
-        print(sent['sent_id'])
-        
-        img_id = ref['image_id']
-        ann_id = ref['ann_id']
-        objs = self.refer.imgToAnns[img_id]
-        obj_ids = [obj['id'] for obj in objs]
-        
-        sub_images = []
-        for obj in objs:
-            x_a = get_bounded_subimage(refer, img_id, obj['id'], xs=224,ys=224, show=False)
-            if x_a is not None:
-                sub_images.append(x_a)
-        num_sub_images = len(sub_images)      
-            
-        text_ids = tokenizer.encode(
-            sent['sent'],
-            padding="max_length",
-            truncation=True,
-            max_length=40,
-            return_special_tokens_mask=True,
-        )
-        text_masks = [1 if text_ids[i]>0 else 0 for i,_ in enumerate(text_ids)]
-        text_labels = [[-100 for i in range(40)]]
-
-        ids = [text_ids for i in range(num_sub_images)]
-        masks = [text_masks for _ in range(num_sub_images)]
-        labels = [text_labels for i in range(num_sub_images)]
-            
-        return_dict = {
-            'ann_id' : ann_id,
-            'image' : sub_images,
-            'obj_ids' : obj_ids,
-            'sent_id' : sent_id,
-            'text' : sent['sent'],
-            'text_ids' : torch.tensor(ids),
-            'text_labels' : torch.tensor(labels),
-            'text_masks' : torch.tensor(masks)
-        }  
-
-        return return_dict
 
 
 # Ref Res with METER
