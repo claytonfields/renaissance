7d4ba84a126654b00d28d293b4f206aa586403a9
claytonfields <clayton.fields01@yahoo.com>
2024-01-04 19:41:28 +0000

    Adjusted setting for task_mlm_itm_deit_electra in meter/config.py

--- meter/config.py
+++ meter/config.py
@@ -127,11 +127,11 @@ def task_mlm_itm_clip_bert():
     
 @ex.named_config
 def task_mlm_itm_deit_electra():
-    exp_name = "mlm_itm"
+    exp_name = "mlm_itm_deit_electra"
     # datasets = ["coco", "vg", "sbu", "gcc"]
     datasets = ["coco", "vg"]
     loss_names = _loss_names({"itm": 1, "mlm": 1})
-    batch_size = 256
+    batch_size = 336
     max_epoch = None
     max_steps = 100000
     warmup_steps = 0.1
--- run.py
+++ run.py
@@ -59,7 +59,7 @@ def main(_config):
         gpus=_config["num_gpus"],
         num_nodes=_config["num_nodes"],
         precision=_config["precision"],
-        # accelerator = 'ddp',
+        accelerator = 'ddp',
         benchmark=True,
         deterministic=True,
         max_epochs=_config["max_epoch"] if max_steps is None else 1000,
