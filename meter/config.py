0abda77382508a57d007ce3bb4be9d90592d6016
claytonfields <claytonfields@u.boisestate.edu>
2024-01-17 14:51:16 -0700

    Added test case for snli to config.py.

--- meter/config.py
+++ meter/config.py
@@ -551,6 +551,105 @@ def freeze_image():
 def freeze_text():
     freeze_text_encoder = True
     
+    
+# ===================== Test Cases ===================== #
+
+# SNLI
+@ex.named_config
+def test_case_finetune_snli():
+    # Settings
+    test_only=False
+    data_root = 'data/arrow/' 
+    num_gpus=12
+    num_nodes=1 
+    per_gpu_batchsize=64 
+    load_path = '/data/clayton/meter/result/mlm_itm_deit_fr_electra_fr_seed0_from_/mlm_itm_deit_fr_electra_fr_is224_ps16_bs336_pgbs84_ts100k/checkpoints'
+    # SNLI-VE
+    exp_name = "test_case_finetune_snli"
+    datasets = ["snli"]
+    loss_names = _loss_names({"snli": 1})
+    # Training Time
+    max_epoch = 1
+    max_steps = 10
+    # Text Encoder
+    text_encoder = "google/electra-small-discriminator"
+    vocab_size = 30522
+    text_encoder_hidden_size = 256
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    # num_layers = 6
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    # Image Encoder Settings
+    image_encoder = "facebook/deit-tiny-patch16-224"
+    image_encoder_hidden_size = 192
+    image_size = 224
+    resolution_before = 224
+    patch_size = 16
+    train_transform_keys = ["imagenet"]
+    val_transform_keys = ["imagenet"]
+    # Training Settings
+    batch_size = 64
+    warmup_steps = 0.1
+    draw_false_image = 0
+    learning_rate = 2e-6
+    lr_mult_head = 10
+    lr_mult_cross_modal = 5
+    max_text_len = 50
+    # Freeze or UnFreeze Encoders
+    freeze_image_encoder = False
+    freeze_text_encoder = False
+
+@ex.named_config
+def test_case_eval_snli():
+    # Settings
+    test_only=True
+    data_root = 'data/arrow/' 
+    num_gpus=1 
+    num_nodes=1 
+    per_gpu_batchsize=64 
+    # load_path = ''
+    # SNLI-VE
+    exp_name = "test_case_finetune_snli"
+    datasets = ["snli"]
+    loss_names = _loss_names({"snli": 1})
+    # Training Time
+    max_epoch = 1
+    max_steps = 1000
+    # Text Encoder
+    text_encoder = "google/electra-small-discriminator"
+    vocab_size = 30522
+    text_encoder_hidden_size = 256
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    # num_layers = 6
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    # Image Encoder Settings
+    image_encoder = "facebook/deit-tiny-patch16-224"
+    image_encoder_hidden_size = 192
+    image_size = 224
+    resolution_before = 224
+    patch_size = 16
+    train_transform_keys = ["imagenet"]
+    val_transform_keys = ["imagenet"]
+    # Training Settings
+    batch_size = 64
+    warmup_steps = 0.1
+    draw_false_image = 0
+    learning_rate = 2e-6
+    lr_mult_head = 10
+    lr_mult_cross_modal = 5
+    max_text_len = 50
+    # Freeze or UnFreeze Encoders
+    freeze_image_encoder = False
+    freeze_text_encoder = False
+    
+    
 
 
 
