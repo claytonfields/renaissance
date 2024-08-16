0e161f8250820b862512303b9afb2ae762fdce80
claytonfields <claytonfields@u.boisestate.edu>
2024-08-16 14:19:19 -0600

    Added several new configurations for model specific finetuning task.

--- renaissance/config.py
+++ renaissance/config.py
@@ -713,7 +713,7 @@ def task_finetune_nlvr2_twotower_deit_electra():
     
 @ex.named_config
 def task_finetune_nlvr2_twotower_dino_tinybert():
-    exp_name = "nlvr2_twotower_deit_electra"
+    exp_name = "nlvr2_twotower_dinos_tinybert"
     model_type = "two-tower"
     datasets = ["nlvr2"]
     loss_names = _loss_names({"nlvr2": 1})
@@ -780,6 +780,36 @@ def task_finetune_vqa_twotower_deit_electra():
     lr_mult_cross_modal = 5
     # Text Settings
     text_encoder = "google/electra-small-discriminator"
+    max_text_len = 50
+    # Image Settins
+    image_encoder = "facebook/deit-tiny-patch16-224"
+    train_transform_keys = ["imagenet"]
+    val_transform_keys = ["imagenet"]
+    image_size = 576
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    
+@ex.named_config
+def task_finetune_vqa_twotower_dinos_tinybert():
+    exp_name = "vqa_twotower_dinos_tinybert"
+    model_type = "two-tower"
+    datasets = ["vqa"]
+    loss_names = _loss_names({"vqa": 1})
+    batch_size = 512
+    max_epoch = 10
+    max_steps = 1e6
+    warmup_steps = 0.1
+    draw_false_image = 0
+    learning_rate = 5e-6
+    val_check_interval = 0.1
+    lr_mult_head = 50
+    lr_mult_cross_modal = 5
+    # Text Settings
+    image_encoder = "facebook/dino-vits16"
     max_text_len = 50
     # Image Settins
     image_encoder = "facebook/deit-tiny-patch16-224"
@@ -1011,6 +1041,35 @@ def task_finetune_ref_twotower_deit_electra():
     num_cross_layer_heads = 4
     cross_layer_mlp_ratio = 4
     cross_layer_drop_rate = 0.1
+    
+@ex.named_config
+def task_finetune_ref_twotower_dinos_tinybert():
+    exp_name = "ref_twotower_dinos_tinybert"
+    model_type = "two-tower"
+    datasets = ["refcoco"]
+    loss_names = _loss_names({"ref": 1})
+    batch_size = 5
+    max_epoch = 5
+    max_steps = 10e6
+    warmup_steps = 0.1
+    draw_false_image = 0
+    learning_rate = 2e-6
+    lr_mult_head = 10
+    lr_mult_cross_modal = 5
+    max_text_len = 40
+    image_size = 224
+    
+    # Encoder Settings
+    text_encoder = "huawei-noah/TinyBERT_General_4L_312D"
+    image_encoder = "facebook/dino-vits16"
+    
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    
 
 @ex.named_config
 def task_finetune_irtr_coco_clip_bert():
