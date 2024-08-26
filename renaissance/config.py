e856b99865e2f634164e97b4c81325688b83997f
claytonfields <claytonfields@u.boisestate.edu>
2024-08-26 10:34:01 -0600

    Added new config for two-tower modeling with vit-base-frozen and electra-base-frozen to renaissance/config.py.

--- renaissance/config.py
+++ renaissance/config.py
@@ -681,6 +681,50 @@ def task_mlm_itm_two_tower_dinos_tinybert():
     image_size = 224
     patch_size = 16
     
+    
+@ex.named_config
+def task_mlm_itm_twotower_vitbase_fr_electrabase_fr():
+    exp_name = "mlm_itm_twotower_vitbase_fr_electrabase_fr"
+    model_type = "two-tower"
+    # datasets = ["coco", "vg", "sbu", "gcc"]
+    datasets = ["coco", "vg"]
+    loss_names = _loss_names({"itm": 1, "mlm": 1})
+    batch_size = 176
+    per_gpu_batchsize = 44
+    max_epoch = None
+    max_steps = 50000
+    warmup_steps = 0.1
+    whole_word_masking = True
+    model_type = "two-tower"
+    # DO NOT Freeze Encoders
+    freeze_image_encoder = True
+    freeze_text_encoder = True
+    # Image settings
+    image_encoder = "google/vit-base-patch16-224"
+    train_transform_keys = ["imagenet"]
+    val_transform_keys = ["imagenet"]
+    image_size = 224
+    # resolution_before = 224
+    patch_size = 16
+    draw_false_image = 1
+    image_only = False
+    # Text Setting
+    text_encoder = "google/electra-base-discriminator"
+    max_text_len = 50
+    whole_word_masking = True # note that whole_word_masking does not work for RoBERTa
+    mlm_prob = 0.15
+    draw_false_text = 0
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    # Optimizer Settings
+    learning_rate = 1e-5
+    val_check_interval = 1.0
+    lr_mult_head = 5
+    lr_mult_cross_modal = 5
 # ===================== Finetuning Tasks===================== #
 @ex.named_config
 def task_finetune_nlvr2():
@@ -943,6 +987,38 @@ def task_finetune_snli_twotower_deit_electra():
     cross_layer_mlp_ratio = 4
     cross_layer_drop_rate = 0.1
 
+
+
+@ex.named_config
+def task_finetune_snli_twotower_vitbase_electrabase():
+    exp_name = "snli_twotower_deit_electra"
+    model_type = "two-tower"
+    datasets = ["snli"]
+    loss_names = _loss_names({"snli": 1})
+    batch_size = 64
+    max_epoch = 5
+    max_steps = 10e6
+    warmup_steps = 0.1
+    draw_false_image = 0
+    learning_rate = 2e-6
+    lr_mult_head = 10
+    lr_mult_cross_modal = 5
+    max_text_len = 50
+    image_size = 384
+    # DO NOT Freeze Encoders
+    freeze_image_encoder = False
+    freeze_text_encoder = False
+
+    # Encoder Settings
+    text_encoder = "google/electra-base-discriminator"
+    image_encoder = "google/vit-base-patch16-224"
+    
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
     
 @ex.named_config
 def task_finetune_snli_onetower():
