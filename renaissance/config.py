5102e6b3b9fd5e22ac5272fe47327949e775f1c8
claytonfields <claytonfields@u.boisestate.edu>
2024-11-12 16:55:43 -0700

    Added configs for swintiny-electratiny to renaissance/config.py.

--- renaissance/config.py
+++ renaissance/config.py
@@ -178,7 +178,7 @@ def pretrain_mlm_itm_twotower_exp2_deittiny_electrasmall():
     freeze_image_encoder = False
     freeze_text_encoder = False
     # Image settings
-    image_encoder = "facebook/deit-tiny-distilled-patch16-224"
+    image_encoder = "facebook/deit-tiny-patch16-224"
     image_size = 224
     patch_size = 16
     draw_false_image = 1
@@ -218,7 +218,7 @@ def pretrain_mlm_itm_twotower_exp2_deitsmall_electrasmall():
     freeze_image_encoder = False
     freeze_text_encoder = False
     # Image settings
-    image_encoder = "facebook/deit-small-distilled-patch16-224"
+    image_encoder = "facebook/deit-tiny-patch16-224"
     image_size = 224
     patch_size = 16
     draw_false_image = 1
@@ -259,7 +259,7 @@ def pretrain_mlm_itm_twotower_exp2_deittiny_electratiny():
     freeze_image_encoder = False
     freeze_text_encoder = False
     # Image settings
-    image_encoder = "facebook/deit-tiny-distilled-patch16-224"
+    image_encoder = "facebook/deit-tiny-patch16-224"
     image_size = 224
     patch_size = 16
     draw_false_image = 1
@@ -299,7 +299,7 @@ def pretrain_mlm_itm_twotower_exp2_deitsmall_electratiny():
     freeze_image_encoder = False
     freeze_text_encoder = False
     # Image settings
-    image_encoder = "facebook/deit-small-distilled-patch16-224"
+    image_encoder = "facebook/deit-small-patch16-224"
     image_size = 224
     patch_size = 16
     draw_false_image = 1
@@ -339,9 +339,9 @@ def pretrain_mlm_itm_twotower_exp2_swintiny_electratiny():
     freeze_image_encoder = False
     freeze_text_encoder = False
     # Image settings
-    image_encoder = "facebook/deit-small-distilled-patch16-224"
+    image_encoder = "microsoft/swin-tiny-patch4-window7-224"
     image_size = 224
-    patch_size = 16
+    patch_size = 4
     draw_false_image = 1
     # Text Setting
     text_encoder = "claytonfields/electra-tiny"
@@ -379,12 +379,12 @@ def pretrain_mlm_itm_twotower_exp2_swintiny_electrasmall():
     freeze_image_encoder = False
     freeze_text_encoder = False
     # Image settings
-    image_encoder = "facebook/deit-small-distilled-patch16-224"
+    image_encoder = "microsoft/swin-tiny-patch4-window7-224"
     image_size = 224
-    patch_size = 16
+    patch_size = 4
     draw_false_image = 1
     # Text Setting
-    text_encoder = "claytonfields/electra-tiny"
+    text_encoder = "google/electra-small-discriminator"
     max_text_len = 50
     whole_word_masking = True # note that whole_word_masking does not work for RoBERTa
     mlm_prob = 0.15
@@ -424,7 +424,7 @@ def finetune_nlvr2_twotower_exp2_deittiny_electrasmall():
     text_encoder = "google/electra-small-discriminator"
     max_text_len = 50
     # Image Settings
-    image_encoder = "facebook/deit-tiny-distilled-patch16-224"
+    image_encoder = "facebook/deit-tiny-patch16-224"
     patch_size = 16
     image_size = 288
     # Cross Layer Settings
@@ -457,8 +457,74 @@ def finetune_nlvr2_twotower_exp2_deittiny_electratiny():
     text_encoder = "claytonfields/electra-tiny"
     max_text_len = 50
     # Image Settings
-    image_encoder = "facebook/deit-tiny-distilled-patch16-224"
+    image_encoder = "facebook/deit-tiny-patch16-224"
     patch_size = 16
+    image_size = 288
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    
+@ex.named_config
+def finetune_nlvr2_twotower_exp2_swintiny_electratiny():
+    exp_name = "nlvr2_twotower_exp2_swintiny_electratiny"
+    model_type = "two-tower"
+    datasets = ["nlvr2"]
+    loss_names = _loss_names({"nlvr2": 1})
+    # Hardware Settings
+    per_gpu_batchsize = 32 
+    num_nodes = 1 
+    num_gpus = 2 
+    # Training Settings
+    batch_size = 256
+    max_epoch = 40  
+    max_steps = 10e6
+    warmup_steps = 0.05
+    draw_false_image = 0
+    learning_rate = 1e-4
+    lr_mult_head = 5  
+    lr_mult_cross_modal = 5 
+    # Text Setting
+    text_encoder = "claytonfields/electra-tiny"
+    max_text_len = 50
+    # Image Settings
+    image_encoder = "microsoft/swin-tiny-patch4-window7-224"
+    patch_size = 4
+    image_size = 288
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    
+@ex.named_config
+def finetune_nlvr2_twotower_exp2_swintiny_electrasmall():
+    exp_name = "nlvr2_twotower_exp2_swintiny_electrasmall"
+    model_type = "two-tower"
+    datasets = ["nlvr2"]
+    loss_names = _loss_names({"nlvr2": 1})
+    # Hardware Settings
+    per_gpu_batchsize = 32 
+    num_nodes = 1 
+    num_gpus = 2 
+    # Training Settings
+    batch_size = 256
+    max_epoch = 40  
+    max_steps = 10e6
+    warmup_steps = 0.05
+    draw_false_image = 0
+    learning_rate = 1e-4
+    lr_mult_head = 5  
+    lr_mult_cross_modal = 5 
+    # Text Setting
+    text_encoder = "google/electra-small-discriminator"
+    max_text_len = 50
+    # Image Settings
+    image_encoder = "microsoft/swin-tiny-patch4-window7-224"
+    patch_size = 4
     image_size = 288
     # Cross Layer Settings
     cross_layer_hidden_size = 256
@@ -490,13 +556,14 @@ def finetune_snli_twotower_exp2_deittiny_electrasmall():
     lr_mult_cross_modal = 5
     max_text_len = 50
     image_size = 384
+    patch_size = 16
     # DO NOT Freeze Encoders
     freeze_image_encoder = False
     freeze_text_encoder = False
 
     # Encoder Settings
     text_encoder = "google/electra-small-discriminator"
-    image_encoder = "facebook/deit-tiny-distilled-patch16-224"
+    image_encoder = "facebook/deit-tiny-patch16-224"
     
     # Cross Layer Settings
     cross_layer_hidden_size = 256
@@ -526,13 +593,88 @@ def finetune_snli_twotower_exp2_deittiny_electratiny():
     lr_mult_cross_modal = 5
     max_text_len = 50
     image_size = 384
+    patch_size = 16
     # DO NOT Freeze Encoders
     freeze_image_encoder = False
     freeze_text_encoder = False
 
     # Encoder Settings
     text_encoder = "claytonfields/electra-tiny"
-    image_encoder = "facebook/deit-tiny-distilled-patch16-224"
+    image_encoder = "facebook/deit-tiny-patch16-224"
+    
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    
+@ex.named_config
+def finetune_snli_twotower_exp2_swintiny_electratiny():
+    exp_name = "snli_twotower_exp2_swintiny_electratiny"
+    model_type = "two-tower"
+    datasets = ["snli"]
+    loss_names = _loss_names({"snli": 1})
+    # Hardware Settings
+    per_gpu_batchsize = 64  
+    num_nodes = 1 
+    num_gpus = 2
+    # Training Settings
+    batch_size = 64
+    max_epoch = 10 
+    max_steps = 10e6
+    warmup_steps = 0.05
+    draw_false_image = 0
+    learning_rate = 1e-4
+    lr_mult_head = 5
+    lr_mult_cross_modal = 5
+    max_text_len = 50
+    image_size = 384
+    patch_size = 4
+    # DO NOT Freeze Encoders
+    freeze_image_encoder = False
+    freeze_text_encoder = False
+
+    # Encoder Settings
+    text_encoder = "claytonfields/electra-tiny"
+    image_encoder = "microsoft/swin-tiny-patch4-window7-224"
+    
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    
+@ex.named_config
+def finetune_snli_twotower_exp2_swintiny_electrasmall():
+    exp_name = "snli_twotower_exp2_swintiny_electrasmall"
+    model_type = "two-tower"
+    datasets = ["snli"]
+    loss_names = _loss_names({"snli": 1})
+    # Hardware Settings
+    per_gpu_batchsize = 64  
+    num_nodes = 1 
+    num_gpus = 2
+    # Training Settings
+    batch_size = 64
+    max_epoch = 10 
+    max_steps = 10e6
+    warmup_steps = 0.05
+    draw_false_image = 0
+    learning_rate = 1e-4
+    lr_mult_head = 5
+    lr_mult_cross_modal = 5
+    max_text_len = 50
+    image_size = 384
+    patch_size = 4
+    # DO NOT Freeze Encoders
+    freeze_image_encoder = False
+    freeze_text_encoder = False
+
+    # Encoder Settings
+    text_encoder = "google/electra-small-discriminator"
+    image_encoder = "microsoft/swin-tiny-patch4-window7-224"
     
     # Cross Layer Settings
     cross_layer_hidden_size = 256
@@ -562,10 +704,11 @@ def finetune_ref_twotower_exp2_deittiny_electrasmall():
     lr_mult_cross_modal = 5
     max_text_len = 40
     image_size = 224
+    patch_size = 16
     
     # Encoder Settings
     text_encoder = "google/electra-small-discriminator"
-    image_encoder = "facebook/deit-tiny-distilled-patch16-224"
+    image_encoder = "facebook/deit-tiny-patch16-224"
     
     # Cross Layer Settings
     cross_layer_hidden_size = 256
@@ -595,10 +738,79 @@ def finetune_ref_twotower_exp2_deittiny_electratiny():
     lr_mult_cross_modal = 5
     max_text_len = 40
     image_size = 224
+    patch_size = 16
     
     # Encoder Settings
     text_encoder = "claytonfields/electra-tiny"
-    image_encoder = "facebook/deit-tiny-distilled-patch16-224"
+    image_encoder = "facebook/deit-tiny-patch16-224"
+    
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    
+@ex.named_config
+def finetune_ref_twotower_exp2_swintiny_electratiny():
+    exp_name = "ref_twotower_exp2_swintiny_electratiny"
+    model_type = "two-tower"
+    datasets = ["refcoco"]
+    loss_names = _loss_names({"ref": 1})
+    # Hardware Settings
+    num_nodes = 1
+    num_gpus = 2
+    per_gpu_batchsize = 10
+    # Training Setttings
+    batch_size = 60
+    max_epoch = 10
+    max_steps = 10e6
+    warmup_steps = 0.05
+    draw_false_image = 0
+    learning_rate = 7.5e-4
+    lr_mult_head = 5
+    lr_mult_cross_modal = 5
+    max_text_len = 40
+    image_size = 224
+    patch_size = 4
+    
+    # Encoder Settings
+    text_encoder = "claytonfields/electra-tiny"
+    image_encoder = "microsoft/swin-tiny-patch4-window7-224"
+    
+    # Cross Layer Settings
+    cross_layer_hidden_size = 256
+    num_cross_layers = 6
+    num_cross_layer_heads = 4
+    cross_layer_mlp_ratio = 4
+    cross_layer_drop_rate = 0.1
+    
+@ex.named_config
+def finetune_ref_twotower_exp2_swintiny_electrasmall():
+    exp_name = "ref_twotower_exp2_swintiny_electrasmall"
+    model_type = "two-tower"
+    datasets = ["refcoco"]
+    loss_names = _loss_names({"ref": 1})
+    # Hardware Settings
+    num_nodes = 1
+    num_gpus = 2
+    per_gpu_batchsize = 10
+    # Training Setttings
+    batch_size = 60
+    max_epoch = 10
+    max_steps = 10e6
+    warmup_steps = 0.05
+    draw_false_image = 0
+    learning_rate = 7.5e-4
+    lr_mult_head = 5
+    lr_mult_cross_modal = 5
+    max_text_len = 40
+    image_size = 224
+    patch_size = 4
+    
+    # Encoder Settings
+    text_encoder = "google/electra-small-discriminator"
+    image_encoder = "microsoft/swin-tiny-patch4-window7-224"
     
     # Cross Layer Settings
     cross_layer_hidden_size = 256
